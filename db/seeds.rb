b8d6139063f00fdf17b0f2a1961b629a97e6be43
Johnson <johnsonphan95@gmail.com>
2019-05-03 18:53:16 -0700

    pre heroku push

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
+Movie.destroy_all
+
 demoUser = User.create(email: "demo@fletnix.com", password: "password")
 tarzan = Movie.create(title: "Tarzan", description: "Swinging deez vines", year: 1999, rating: "G", run_time: "1h 28m") 
+# arzan1 = Movie.create(title: "Tarzan", description: "Swinging deez vines", year: 1999, rating: "G", run_time: "1h 28m") 
+# tarzan1_image = open('s3 html')
+# tarzan1.photo.attach(io: tarzan1_image, filename: "something")t
